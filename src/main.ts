b003021df248af902c0eb7ed7c1ed7a3681e567c
anticher <anticherq@gmail.com>
2022-08-11 12:32:53 +0300

    feat: add uncaughtException and unhandledRejection logging

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.log(`Unhandled Rejection at:', ${promise}, 'reason:', ${reason}`);
+});
+
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`);
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
